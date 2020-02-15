3f8c669035f84c5bbe56a3696203f78daa8dc0aa
Dante <jazzy.d1302@outlook.com>
2020-02-15 14:02:17 -0500

    Changes

--- src/main/java/frc/robot/subsystems/colourWheel.java
+++ src/main/java/frc/robot/subsystems/colourWheel.java
@@ -28,6 +28,13 @@ public class colourWheel extends SubsystemBase {
   private double redValue = 0;
   private double greenValue = 0;
   private double blueValue = 0;
+  private double redRange=0.02;
+  private double blueRange=0.02;
+  private double greenRange=0.02;
+  private double red[] = {0.47,0.36,0.13};
+  private double blue[] = {0.15,0.42,0.42};
+  private double green[] = {0.2,0.5,0.2};
+  private double yellow[] = {0.32,0.53,0.24};
 
   public colourWheel() {
    
@@ -66,20 +73,20 @@ public class colourWheel extends SubsystemBase {
     SmartDashboard.putNumber("Red", detectedColour.red);
     SmartDashboard.putNumber("Green", detectedColour.green);
     SmartDashboard.putNumber("Blue", detectedColour.blue);
-    //Blue
-    if(redValue >= 0.15 && redValue <= 0.20 && blueValue >= 0.39 && blueValue <= 0.45 && greenValue >= 0.39 && greenValue <= 0.45){
+    //Red
+    if(redValue >= red[0] + redRange && redValue <= red[0] - redRange && blueValue >= red[1] + blueRange && blueValue <= red[1] - blueRange && greenValue >= red[2]+greenRange && greenValue <= red[2]-greenRange){
       currentColour = 0;
     }
     //Yellow
-    if(redValue >= 0.15 && redValue <= 0.20 && blueValue >= 0.39 && blueValue <= 0.45 && greenValue >= 0.39 && greenValue <= 0.45){
+    if(redValue >=  yellow[0] + redRange && redValue <= yellow[0] - redRange && blueValue >= yellow[1] + blueRange && blueValue <= yellow[1] - blueRange && greenValue >= yellow[2] + greenRange && greenValue <= yellow[2] - greenRange){
       currentColour = 1;
     }
-    //Red
-    if(redValue >= 0.15 && redValue <= 0.20 && blueValue >= 0.39 && blueValue <= 0.45 && greenValue >= 0.39 && greenValue <= 0.45){
+    //Blue
+    if(redValue >=  blue[0] + redRange && redValue <= blue[0] - redRange && blueValue >= blue[1] + blueRange && blueValue <= blue[1] - blueRange && greenValue >= blue[2] + greenRange && greenValue <= blue[2] - greenRange){
       currentColour = 2;
     }
     //Green
-    if(redValue >= 0.15 && redValue <= 0.20 && blueValue >= 0.39 && blueValue <= 0.45 && greenValue >= 0.39 && greenValue <= 0.45){
+    if(redValue >=  green[0] + redRange && redValue <= green[0] - redRange && blueValue >= green[1] + blueRange && blueValue <= green[1] - blueRange && greenValue >= green[2] + greenRange && greenValue <= green[2] - greenRange){
       currentColour = 3;
     }
     if(currentColour == colour){
