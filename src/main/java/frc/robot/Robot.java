61593826e4996c0768677f89830e781d6d3374f2
Gabriel <gabriel.i.rueda@gmail.com>
2020-03-02 20:46:35 -0500

    robots

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -59,42 +59,9 @@ public class Robot extends TimedRobot {
     autoChooser = new SendableChooser();
     autoChooser.addDefault("Furthest From Wall - Auto 1", m_robotContainer.getAutonomousCommand());
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    autoChooser.addDefault("In front of Powerport - Auto 2", m_robotContainer.getAutonomousCommand());
-=======
-    CamServer.camInit();
->>>>>>> Stashed changes
-=======
-<<<<<<< HEAD
     autoChooser.addDefault("In front of Powerport - Auto 2", A_InFrontofPowerport());
     autoChooser.addDefault("Middle - Auto 3", new );
     autoChooser.addObject("Auto mode chooser", autoChooser);
-=======
-    CamServer.camInit();
->>>>>>> master
->>>>>>> Stashed changes
-=======
-    autoChooser.addDefault("In front of Powerport - Auto 2", A_InFrontofPowerport());
-    autoChooser.addDefault("Middle - Auto 3", new );
-    autoChooser.addObject("Auto mode chooser", autoChooser);
->>>>>>> Stashed changes
-=======
-    autoChooser.addDefault("In front of Powerport - Auto 2", A_InFrontofPowerport());
-    autoChooser.addDefault("Middle - Auto 3", new );
-    autoChooser.addObject("Auto mode chooser", autoChooser);
->>>>>>> Stashed changes
-=======
-    autoChooser.addDefault("In front of Powerport - Auto 2", A_InFrontofPowerport());
-    autoChooser.addDefault("Middle - Auto 3", new );
-    autoChooser.addObject("Auto mode chooser", autoChooser);
->>>>>>> Stashed changes
-=======
-    autoChooser.addDefault("In front of Powerport - Auto 2", A_InFrontofPowerport());
-    autoChooser.addDefault("Middle - Auto 3", new );
-    autoChooser.addObject("Auto mode chooser", autoChooser);
->>>>>>> Stashed changes
   }
 
   /**
